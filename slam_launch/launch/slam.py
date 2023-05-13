34341b3e805b81ff92f9fbaecbd06459e608960a
Cynthia <yuyingli123@gmail.com>
2023-05-13 10:46:00 -0400

    updated launch file

--- slam_launch/launch/slam.py
+++ slam_launch/launch/slam.py
@@ -3,12 +3,10 @@ from launch_ros.actions import Node
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 import launch.actions
-from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
 import os
-from launch.substitutions.launch_configuration import LaunchConfiguration
 
 def generate_launch_description():
 
@@ -52,6 +50,7 @@ def generate_launch_description():
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
+        output="screen",
         name="rviz2",
         arguments=["-d", rviz_config_file]
     )
